e1012aeb014a37bba7f495fb7f023c47bc83dd21
NaturalH <marco.mmtz@proton.me>
2025-05-28 02:03:22 -0600

    Bruh

--- models/libros_autores.go
+++ models/libros_autores.go
@@ -4,6 +4,6 @@ type LibrosAutores struct {
 	LibroAutorID uint    `gorm:"primaryKey"`
 	LibroID      uint    `gorm:"uniqueIndex:udx_libro_autor_id"`
 	Libro        Libros  `gorm:"references:LibroID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
-	AutorID      uint    `gorm:"uniqueIndex:udx_autor_id"`
+	AutorID      uint    `gorm:"uniqueIndex:udx_libro_autor_id"`
 	Autor        Autores `gorm:"references:AutorID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
 }
